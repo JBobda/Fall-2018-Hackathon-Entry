46e75719e5eb53046bf8ddf01b0aa8596f5706ef
JBobda <J.Bobda@g.fayar.net>
2018-09-22 12:51:26 -0500

    small update

---
+++ app/src/main/java/dev/hackathon/hackermen/hackathonentry/InputManager.kt
@@ -0,0 +1,18 @@
+package dev.hackathon.hackermen.hackathonentry
+
+import android.support.annotation.IntegerRes
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import dev.hackathon.hackermen.hackathonentry.R.id.seekBar
+
+class InputManager : AppCompatActivity(){
+
+    var barOne = 0
+    var barTwo = 0
+    var location = ""
+
+    fun onConfirmClick(view: View){
+        setContentView(R.layout.activity_question);
+        //location =
+    }
+}

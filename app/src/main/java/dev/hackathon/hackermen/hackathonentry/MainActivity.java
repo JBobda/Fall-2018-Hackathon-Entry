b6150a4ff3a805ce249a9077ca9bd7c599a34519
eahelf <e.helf@g.fayar.net>
2018-09-22 01:59:46 -0500

    Menu/settings system not functional but 0 errors

--- app/src/main/java/dev/hackathon/hackermen/hackathonentry/MainActivity.java
+++ app/src/main/java/dev/hackathon/hackermen/hackathonentry/MainActivity.java
@@ -1,14 +1,24 @@
 package dev.hackathon.hackermen.hackathonentry;
 
+        import android.support.v4.view.GravityCompat;
+        import android.support.v4.widget.DrawerLayout;
+        import android.support.v7.app.ActionBarDrawerToggle;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Utilities().APICalls(); //calls the API
+        //new Utilities().APICalls(); //calls the API
+
+
     }
+
+
 }

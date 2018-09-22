32f460ce4b09a693adbd92d1362e716981d52a50
faups <lukefaupel@gmail.com>
2018-09-21 22:59:16 -0500

    Merge remote-tracking branch 'origin/master' asdfasdf # Conflicts: #	app/src/main/java/dev/hackathon/hackermen/hackathonentry/MainActivity.java

--- app/src/main/java/dev/hackathon/hackermen/hackathonentry/MainActivity.java
+++ app/src/main/java/dev/hackathon/hackermen/hackathonentry/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Utilities().APICalls();
+        new Utilities().APICalls(); //calls the API
     }
 }
